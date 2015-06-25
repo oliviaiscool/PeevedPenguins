38b33bb883b0c60fdab2fc1bde106bf0bcae84a4
Olivia Ross <ross.mollie@gmail.com>
2015-06-25 14:30:23 -0400

    Added more sprite animations. Fixed scrolling and allowed multiple attempts.

---
+++ PeevedPenguins.spritebuilder/Source/WaitingPenguin.swift
@@ -0,0 +1,13 @@
+//
+//  WaitingPenguin.swift
+//  PeevedPenguins
+//
+//  Created by Olivia Ross on 6/25/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+import UIKit
+
+class WaitingPenguin: CCSprite {
+   
+}
